8f6c03886c44fa53fd7b66ff63367a5c3a71e54c
yoblig <gilboyk@gmail.com>
2017-01-30 01:04:05 -0600

    added sections of card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 
 const Card = (props) => {
     return (
-        <View>
-
+        <View style={styles.containerStyle}>
+            {props.children}
         </View>
     );
 };
@@ -17,7 +17,12 @@ const styles = {
         borderBottomWidth: 0,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
-
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
     }
 };
 

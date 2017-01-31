266a11c05123adf68bd7d5cd8f7145febe8bd3bd
yoblig <gilboyk@gmail.com>
2017-01-31 00:40:38 -0600

    button will now display text and link

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
@@ -22,7 +22,9 @@ const AlbumDetail = ({ album }) => {
                 <Image style={imageStyle} source={{ uri: image }} />
             </CardSection>
             <CardSection>
-                <Button />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Today
+                </Button>
             </CardSection>
         </Card>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, children }) => {
+    const {
+        buttonStyles,
+        textStyles
+    } = styles;
+
     return (
-        <Text>Buy Now</Text>
+        <TouchableOpacity onPress={onPress} style={buttonStyles}>
+            <Text style={textStyles}>
+                {children}
+            </Text>
+        </TouchableOpacity>
     );
 };
+
+const styles = {
+    textStyles: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonStyles: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginRight: 5,
+        marginLeft: 5
+    }
+}
 
 export default Button;
